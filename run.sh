b87c8a99a9cb6503b4966e2cb0f12a1927124f09
mikewschmidt <michaelwschmidt@gmail.com>
2025-09-13 21:37:48 -0500

    added 1 month plus small updates

--- run.sh
+++ run.sh
@@ -3,9 +3,13 @@
 
 export CUDA_LAUNCH_BLOCKING=1
 
+python -m pip install --upgrade pip
+
 pip install -r requirements.txt
 
 pip install --upgrade torch torchvision torchaudio
+
+#python testGPU.py
 
 python main.py
 
